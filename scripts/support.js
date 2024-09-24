aa7441f3f53923fad923b2110d10649d891dc62c
88byte <flaviod022@gmail.com>
2024-09-23 19:56:57 -0700

    deal card functions

---
+++ scripts/support.js
@@ -0,0 +1,53 @@
+support.js
+// Import Firebase functions from SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { 
+    getAuth, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword, 
+    signInWithPopup, 
+    GoogleAuthProvider, 
+    signOut, 
+    updateProfile, 
+    updateEmail, 
+    updatePassword, 
+    deleteUser,
+    onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { 
+    getFirestore, 
+    doc, 
+    addDoc, 
+    setDoc, 
+    getDoc, 
+    deleteDoc, 
+    onSnapshot,
+    updateDoc,
+    collection, // Import collection
+    query,      // Import query
+    where,      // Import where
+    getDocs     // Import getDocs
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
+import { 
+    getStorage, 
+    ref, 
+    uploadBytes, 
+    getDownloadURL 
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-storage.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyALOLixHMInPOXN0x1PIzoUQkIn81Iq96g",
+    authDomain: "bizbuybox-webapp-d4772.firebaseapp.com",
+    projectId: "bizbuybox-webapp-d4772",
+    storageBucket: "bizbuybox-webapp-d4772.appspot.com",
+    messagingSenderId: "822306028352",
+    appId: "1:822306028352:web:3e410ae31890ba5d4658a5",
+    measurementId: "G-CWHPBN196R"
+};
+
+
+// Initialize Firestore
+const db = getFirestore();
+window.auth = getAuth(); 
+window.storage = getStorage(); 
